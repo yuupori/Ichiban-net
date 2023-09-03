07134572f83dfe7810996747dc9e8e4527755af6
fujimotoyusei <mukkumukku517@icloud.com>
2023-09-04 00:08:08 +0900

    a

---
+++ login.php
@@ -0,0 +1,37 @@
+<?php
+
+    echo "a";
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style1.css">
+    <title>Document</title>
+</head>
+<body>
+  <a href="index.html"><h1>一番くじ掲示板</h1></a>
+    <div class="sign">
+        <div class="container">
+          <div class="panel">
+            <h2>ログイン</h2>
+            <form action="/login" method="post">
+              <p>メールアドレス</p>
+              <input type="text" name="email">
+              <p>パスワード</p>
+              <input type="password" name="password">
+              <input type="submit" value="ログイン">
+              <p></p><a href="signin.html">新規登録</a></p>
+              <p><a href="index.html">戻る</a></p>
+            </form>
+          </div>
+        </div>
+      </div>
+    
+</body>
+</html>
