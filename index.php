9ce6d30907205cc62c08976d31d25b6d73574300
fujimotoyusei <mukkumukku517@icloud.com>
2023-09-04 00:20:00 +0900

    a

---
+++ index.php
@@ -0,0 +1,156 @@
+
+
+
+
+
+
+
+<!DOCTYPE html>
+
+
+
+
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ichiban-net</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1><marquee behavior="scroll" direction="lift">一番くじ掲示板</marquee></h1>
+    <div>
+    <img src="https://ogre.natalie.mu/media/news/comic/2019/0102/img_174021_18.jpg?imwidth=750&imdensity=1" class="img"></div><br>
+    <a href="https://1kuji.com">一番くじ公式サイト</a> <br>
+    <div class="link1">
+    <a href="https://1kuji.com/products/db_z3" class="link1">一番くじ ドラゴンボール</a></div><br>
+
+    <?php
+
+    ?>
+    
+    <a href="login.php">ログイン・新規登録</a>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <section id="area1">
+    <h2>掲示板</h2></section>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+
+    <div><li><a href="#area1">掲示板へ</a></div></li>
+    
+
+</body>
+</html>
