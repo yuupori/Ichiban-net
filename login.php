4d21a5a1408138269f87dad5b407aa7a78834926
yusei <130953255+yuupori@users.noreply.github.com>
2023-09-11 11:24:42 +0900

    Update login.php

--- login.php
+++ login.php
@@ -1,38 +1,49 @@
 <?php
+session_start();
 
-$name = $_POST['name'];
-$mail = $_POST['mail'];
-$pass = password_hash($_POST['pass'], PASSWORD_DEFAULT);
-$dsn = "mysql:host=localhost; dbname=xxx; charset=utf8";
-$username = "xxx";
-$password = "xxx";
-try {
-    $dbh = new PDO($dsn, $username, $password);
-} catch (PDOException $e) {
-    $msg = $e->getMessage();
+require_once '../classes/UserLogic.php';
+
+
+
+$err = [];
+
+
+if(!$email = filter_input(INPUT_POST, 'email')) {
+    $err['email'] = 'メールアドレスを記入してください';
+}
+if(!$password = filter_input(INPUT_POST, 'password')
+) {
+    $err['password'] = 'パスワードを記入してください'; 
 }
 
 
-$sql = "SELECT * FROM users WHERE mail = :mail";
-$stmt = $dbh->prepare($sql);
-$stmt->bindValue(':mail', $mail);
-$stmt->execute();
-$member = $stmt->fetch();
-if ($member['mail'] === $mail) {
-    $msg = '同じメールアドレスが存在します。';
-    $link = '<a href="signup.php">戻る</a>';
-} else {
+
+if (count($err) > 0) {
+    $_SESSION = $err;
+    header('Location: login.php');
+    return;
     
-    $sql = "INSERT INTO users(name, mail, pass) VALUES (:name, :mail, :pass)";
-    $stmt = $dbh->prepare($sql);
-    $stmt->bindValue(':name', $name);
-    $stmt->bindValue(':mail', $mail);
-    $stmt->bindValue(':pass', $pass);
-    $stmt->execute();
-    $msg = '会員登録が完了しました';
-    $link = '<a href="login.php">ログインページ</a>';
-}
-?>
 
-<h1><?php echo $msg; ?></h1><!--メッセージの出力-->
-<?php echo $link; ?>
+}
+
+$result = UserLogic::login($email, $password);
+if (!$result) {
+    header('Location: login_form.php');
+    return;
+}
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ログイン完了</title>
+</head>
+<body>
+<h2>ログイン完了</h2>
+    <p>ログインしました</p>
+    <a href="./mypage.php">マイページへ</a>
+</body>
+</html>
